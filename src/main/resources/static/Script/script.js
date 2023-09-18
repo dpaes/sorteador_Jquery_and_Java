da4b1a444f3ce730f03d6613c233e9047fddcc86
Daniel Paes <daniel_3xd@hotmail.com>
2023-09-18 01:10:13 -0300

    bad request problem persists

--- src/main/resources/static/Script/script.js
+++ src/main/resources/static/Script/script.js
@@ -1,22 +1,38 @@
 let qtdNumeros = $("#qtdSorteio").val();
 let lowerRangeSorteio = $("#lowerRangeSorteio").val();
 let higherRangeSorteio = $("#higherRangeSorteio").val();
-let ordemCrescente = $("#ordemCrescente").checked;
-let naoRepetirNumeros = $("#naoRepetir").checked;
+let ordemCrescente = $("#ordemCrescente").prop("checked");
+let naoRepetirNumeros = $("#naoRepetir").prop("checked");
+
+console.log("qtdNumeros: "+qtdNumeros)
+console.log("lowerRangeSorteio: "+lowerRangeSorteio)
+console.log("higherRangeSorteio: "+higherRangeSorteio)
+console.log("ordemCrescente: "+ordemCrescente)
+console.log("naoRepetirNumeros: "+naoRepetirNumeros)
+
+let dataSorteio = new Date().toLocaleDateString();
+let gmtSorteio = new Date().toLocaleTimeString();
+
+let dados = {
+    qtdNumeros: parseInt(qtdNumeros),
+    lowerRangeSorteio: parseInt(lowerRangeSorteio),
+    higherRangeSorteio: parseInt(higherRangeSorteio),
+    ordemCrescente: ordemCrescente,
+    naoRepetirNumeros: naoRepetirNumeros
+};
 
 $("#submit").click(function(){
     $.ajax({
         type: "post",
         url: "/",
-        data: {
-            qtdNumeros: qtdNumeros,
-            lowerRangeSorteio: lowerRangeSorteio,
-            higherRangeSorteio: higherRangeSorteio,
-            ordemCrescente: ordemCrescente,
-            naoRepetirNumeros: naoRepetirNumeros
-        },
-        success: function(data){
-            $('#resultado').html(data);
+        contentType: "application/json",
+        data: JSON.stringify(dados),
+        success: function(dados){
+            $('#resultado').html(dados);
+            $('#dataSorteio').html(dataSorteio);
+            $('#gmtSorteio').html(gmtSorteio);
+            $('#qtdSorteadaValue').html(dados.qtdNumeros);
+            $('#rangesValue').html(dados.lowerRangeSorteio + " - " + dados.higherRangeSorteio);
         },
     });
 });
