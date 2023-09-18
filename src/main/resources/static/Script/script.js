d47212cb78869495a89f5643f04e1a99a26da59a
Daniel Paes <77445296+dpaes@users.noreply.github.com>
2023-09-18 18:25:06 -0300

    Update script.js

--- src/main/resources/static/Script/script.js
+++ src/main/resources/static/Script/script.js
@@ -1,27 +1,26 @@
-let qtdNumeros = $("#qtdSorteio").val();
-let lowerRangeSorteio = $("#lowerRangeSorteio").val();
-let higherRangeSorteio = $("#higherRangeSorteio").val();
-let ordemCrescente = $("#ordemCrescente").prop("checked");
-let naoRepetirNumeros = $("#naoRepetir").prop("checked");
-
-console.log("qtdNumeros: "+qtdNumeros)
-console.log("lowerRangeSorteio: "+lowerRangeSorteio)
-console.log("higherRangeSorteio: "+higherRangeSorteio)
-console.log("ordemCrescente: "+ordemCrescente)
-console.log("naoRepetirNumeros: "+naoRepetirNumeros)
-
-let dataSorteio = new Date().toLocaleDateString();
-let gmtSorteio = new Date().toLocaleTimeString();
-
-let dados = {
-    qtdNumeros: parseInt(qtdNumeros),
-    lowerRangeSorteio: parseInt(lowerRangeSorteio),
-    higherRangeSorteio: parseInt(higherRangeSorteio),
-    ordemCrescente: ordemCrescente,
-    naoRepetirNumeros: naoRepetirNumeros
-};
-
 $("#submit").click(function(){
+    let qtdNumeros = $("#qtdSorteio").val();
+    let lowerRangeSorteio = $("#lowerRangeSorteio").val();
+    let higherRangeSorteio = $("#higherRangeSorteio").val();
+    let ordemCrescente = $("#ordemCrescente").prop("checked");
+    let naoRepetirNumeros = $("#naoRepetir").prop("checked");
+
+    let dados = {
+        qtdNumeros: parseInt(qtdNumeros),
+        lowerRangeSorteio: parseInt(lowerRangeSorteio),
+        higherRangeSorteio: parseInt(higherRangeSorteio),
+        ordemCrescente: ordemCrescente,
+        naoRepetirNumeros: naoRepetirNumeros
+    };
+
+    let dataSorteio = new Date().toLocaleDateString();
+    let gmtSorteio = new Date().toLocaleTimeString();
+    
+    console.log("qtdNumeros: "+qtdNumeros)
+    console.log("lowerRangeSorteio: "+lowerRangeSorteio)
+    console.log("higherRangeSorteio: "+higherRangeSorteio)
+    console.log("ordemCrescente: "+ordemCrescente)
+    console.log("naoRepetirNumeros: "+naoRepetirNumeros)
     $.ajax({
         type: "post",
         url: "/",
